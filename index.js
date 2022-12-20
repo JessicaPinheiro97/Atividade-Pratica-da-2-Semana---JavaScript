faee213febd5ea22d0aef5724e416b7a84309968
Jéssica Pinheiro <109962238+JessicaPinheiro97@users.noreply.github.com>
2022-12-20 01:40:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const prompt = require('prompt-sync')();
+
+idade = parseInt(prompt("Digite sua idade: "));
+tempo = parseInt(prompt("Digite seu tempo de serviço: "));
+salario = parseFloat(prompt("Digite seu último salário: R$"));
+
+if (idade >= 65 || tempo >= 30 || (idade >= 60 && tempo >= 25)) {
+  console.log("Você pode se aposentar!");
+  if (tempo > 20) {
+    var aposentadoria = salario * 0.80
+    if (aposentadoria < 1212.00) {
+      aposentadoria = 1212.00
+    }
+    else if (aposentadoria > 7087.22) {
+      aposentadoria = 7087.22
+    }
+    console.log("Seu salário de aposentado será de: R$", aposentadoria);
+  }
+
+  else if (tempo <= 20) {
+    var aposentadoria = salario * 0.60
+    if (aposentadoria < 1212.00) {
+      aposentadoria = 1212.00
+    }
+    else if (aposentadoria > 7087.22) {
+      aposentadoria = 7087.22
+    }
+    console.log("Seu salário de aposentado será de: R$", aposentadoria);
+  }
+}
+
+else {
+  console.log("Você não pode se aposentar");
+}
+
+// ORIENTAÇÕES PARA A ATIVIDADE:
+
+// Implementar o comando de decisão para verificar se o cidadão pode, ou não, se aposentar
+
+// Implementar os comandos de decisão aninhados para calcular o salário de aposentadoria (80% ou 60%)
+
+// Implementar os comandos de decisão aninhados para garantir o salário de aposentadoria dentro dos limites inferior (R$ 1212,00) e superior (R$ 7087,22)
+
